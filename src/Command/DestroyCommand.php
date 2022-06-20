81db3cea51425019dd6ca94d76fbc481bb16e65e
Marco Schuster <m.schuster91@googlemail.com>
2022-06-20 11:56:58 +0200

    add a command to completely tear down the docker-compose environment

---
+++ src/Command/DestroyCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\ConfigFileService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+
+class DestroyCommand extends Command {
+    protected static $defaultName = "quickstart:destroy";
+    /**
+     * @var ConfigFileService
+     */
+    private ConfigFileService $configFileService;
+
+    public function __construct(ConfigFileService $configFileService, string $name = null) {
+        parent::__construct($name);
+        $this->configFileService = $configFileService;
+    }
+
+    protected function configure() {
+        $this
+            ->setDescription("Destroy all containers required by the project")
+            ->setHelp("This command destroys all containers and their data.");
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $cwd = getcwd();
+        $output->writeln("Attempting to load " . $cwd . DIRECTORY_SEPARATOR . ConfigFileService::CONFIG_FILE);
+        $config = $this->configFileService->load($cwd . DIRECTORY_SEPARATOR . ConfigFileService::CONFIG_FILE);
+
+        $command = [
+            "docker-compose",
+            "-f",
+            "docker/docker-compose.yml",
+            "-p",
+            $config->get("project.name"),
+            "down",
+        ];
+
+        $process = new Process($command);
+        $process->setTimeout(null);
+        $process->mustRun(function ($type, $buffer) use ($output) {
+            $output->write($buffer);
+        });
+        return self::SUCCESS;
+    }
+}
